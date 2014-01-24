88d8b1b528ba305b36857088ae231eaa5b9318c3
Paco112 <93352@supinfo.com>
2014-01-24 18:37:51 +0100

    Test login

--- server/controllers/user_socket.js
+++ server/controllers/user_socket.js
@@ -2,8 +2,9 @@ if (typeof define !== 'function') {
     var define = (require('amdefine'))(module);
 }
 define([
-    '../utils/tools_socket'
-], function (tools) {
+    '../utils/tools_socket',
+    '../lib/mongoose'
+], function (tools,mongoose) {
     'use strict';
 
     return {
@@ -15,7 +16,21 @@ define([
         },
 
         submitLogin: function(req){
-            req.io.respond(true);
+            var User = mongoose.models.user;
+            var player = req.data.player;
+            User.findOne({player: player}, function (err, res) {
+                // si le model est null
+                console.log(err)
+                if (res) {
+                    console.log('res ---> ', res);
+                    req.session.user = res.toObject();
+                    console.log('toObject ---> ', res.toObject());
+                    if(req.session.save()) {
+                        return req.io.respond(true);
+                    }
+                }
+                return req.io.respond(false);
+            });
         }
     };
 });
