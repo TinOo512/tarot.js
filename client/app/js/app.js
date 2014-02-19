dbc8d9df318cc6ec51632d0bd8476b2b56bb37a2
etienne <106606@supinfo.com>
2014-02-19 16:51:42 +0100

    move get-player into the rootCtrl instead of the run function

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -4,34 +4,12 @@
 
 var tarotApp = angular.module('tarotApp', [
   'ngRoute',
-  'SocketService',
   'RootCtrl',
   'HomeCtrl',
   'GameCtrl',
   'GamesHistoryCtrl',
   'AboutCtrl',
-  'UserModel',
-  'LoginFormDirectives'
 ]);
-
-tarotApp.run(['$rootScope', 'Socket', 'User', function($rootScope, Socket, User){
-    //todo: rename Player to User
-    console.log('TarotApp');
-    $rootScope.User = User;
-
-    Socket.emit('player/get-player', {}, function (rep) {
-        if (rep.success)
-            $rootScope.User = rep.user;
-        else
-            $rootScope.User = User;
-    });
-
-    $rootScope.submitLogin = function(User){
-        Socket.emit('user/submit-login', {user:User}, function (rep) {
-            console.log(rep);
-        });
-    };
-}]);
 
 tarotApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/home.html'});
--- client/app/js/common/RootCtrl.js
+++ client/app/js/common/RootCtrl.js
@@ -2,9 +2,27 @@
 
 /* RootCtrl Controllers */
 
-var rootCtrl = angular.module('RootCtrl', []);
+var rootCtrl = angular.module('RootCtrl', [
+    'LoginFormDirectives',
+    'SocketService',
+    'UserModel',
+]);
 
-rootCtrl.controller('RootCtrl', ['$scope',
-    function($scope) {
+rootCtrl.controller('RootCtrl', ['$scope', 'Socket', 'User',
+    function($scope, Socket, User) {
         console.log('RootCtrl');
+        $scope.User = User;
+
+        Socket.emit('player/get-player', {}, function (rep) {
+            if (rep.success)
+                $scope.User = rep.user;
+            else
+                $scope.User = User;
+        });
+
+        $scope.submitLogin = function(User){
+            Socket.emit('user/submit-login', {user:User}, function (rep) {
+                console.log(rep);
+            });
+        };
     }]);
